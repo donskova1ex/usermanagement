c715f32fe41e58ad0cee9a7e83d2ca51ef694698
Donskov <donskov_a@icloud.com>
2024-12-17 08:04:30 +0500

    create migrations

---
+++ migrations/20241217025312_persons.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS persons (
+    uuid VARCHAR(36) unique,
+    id SERIAL PRIMARy KEY,
+    first_name VARCHAR(32),
+    last_name VARCHAR(32),
+    full_name VARCHAR(64),
+    email VARCHAR(64));
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS persons;
+-- +goose StatementEnd
